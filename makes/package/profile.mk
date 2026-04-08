feffaea86a800b05b2577dc091f34e5735155480
bijan <mousavi.bijan@gmail.com>
2026-04-09 01:44:22 +0200

    refactor(make): share package boilerplate modules

--- makes/package/profile.mk
+++ makes/package/profile.mk
@@ -1,6 +1,1 @@
-PACKAGE_PROFILE_MAKEFILE ?= $(abspath $(firstword $(MAKEFILE_LIST)))
-PACKAGE_MAKEFILE_DIR ?= $(abspath $(dir $(PACKAGE_PROFILE_MAKEFILE)))
-PROJECT_DIR ?= $(CURDIR)
-PROJECT_SLUG ?= $(notdir $(abspath $(PROJECT_DIR)))
-
-include $(PACKAGE_MAKEFILE_DIR)/../env.mk
+include $(abspath $(dir $(lastword $(MAKEFILE_LIST))))/../bijux-py/package-profile.mk
