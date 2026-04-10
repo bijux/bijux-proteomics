f714e60d2ae9a51e3aceb2be3007b8027aeb24c9
bijan <mousavi.bijan@gmail.com>
2026-04-10 04:57:28 +0200

    fix(packages): complete proteomics check toolchain

--- makes/packages/bijux-proteomics-core.mk
+++ makes/packages/bijux-proteomics-core.mk
@@ -1,6 +1,6 @@
 include $(abspath $(dir $(firstword $(MAKEFILE_LIST))))/../proteomics-package.mk
 
 PACKAGE_IMPORT_NAME := bijux_proteomics
-PACKAGE_INSTALL_PYTHON_PACKAGES = "$(MONOREPO_ROOT)/packages/bijux-proteomics-dev[dev]" $(MONOREPO_ROOT)/packages/bijux-proteomics-foundation
+PACKAGE_INSTALL_PYTHON_PACKAGES = "$(MONOREPO_ROOT)/packages/bijux-proteomics-dev[dev]" $(MONOREPO_ROOT)/packages/bijux-proteomics-foundation $(MONOREPO_ROOT)/packages/bijux-proteomics-intelligence $(MONOREPO_ROOT)/packages/bijux-proteomics-knowledge $(MONOREPO_ROOT)/packages/bijux-proteomics-lab
 
 include $(abspath $(dir $(firstword $(MAKEFILE_LIST))))/../bijux-py/package.mk
