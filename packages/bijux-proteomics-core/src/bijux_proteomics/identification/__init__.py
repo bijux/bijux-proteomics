98c38da215a9e3e16fd94b4a8bba9c993ca4a7d7
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-13 16:23:02 +0200

    feat(identification): add peptide evidence review

--- packages/bijux-proteomics-core/src/bijux_proteomics/identification/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/identification/__init__.py
@@ -10,6 +10,7 @@ from bijux_proteomics.identification.confidence import *  # noqa: F401,F403
 from bijux_proteomics.identification.contaminant_audit import *  # noqa: F401,F403
 from bijux_proteomics.identification.contracts import *  # noqa: F401,F403
 from bijux_proteomics.identification.evidence_level_fdr_review import *  # noqa: F401,F403
+from bijux_proteomics.identification.peptide_evidence_review import *  # noqa: F401,F403
 from bijux_proteomics.identification.parsimony_review import *  # noqa: F401,F403
 from bijux_proteomics.identification.picked_protein_fdr_review import *  # noqa: F401,F403
 from bijux_proteomics.identification.protein_grouping_review import *  # noqa: F401,F403
