7e03ccbc5b5692af2fa78faf4d7e51da3fd6eb7d
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-13 14:24:45 +0200

    feat(identification): add diann report import surfaces

--- packages/bijux-proteomics-core/src/bijux_proteomics/identification/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/identification/__init__.py
@@ -10,6 +10,7 @@ from bijux_proteomics.identification.confidence import *  # noqa: F401,F403
 from bijux_proteomics.identification.contaminant_audit import *  # noqa: F401,F403
 from bijux_proteomics.identification.contracts import *  # noqa: F401,F403
 from bijux_proteomics.identification.search_adapters import *  # noqa: F401,F403
+from bijux_proteomics.identification.diann_import import *  # noqa: F401,F403
 from bijux_proteomics.identification.maxquant_import import *  # noqa: F401,F403
 from bijux_proteomics.identification.comet_import import *  # noqa: F401,F403
 from bijux_proteomics.identification.fragpipe_import import *  # noqa: F401,F403
