657c655b81312a2c5be272b4b373181509a11017
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-22 12:06:56 +0200

    feat(workflow): add diann count fidelity benchmark

--- packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from bijux_proteomics.workflow.blueprint import *  # noqa: F401,F403
 from bijux_proteomics.workflow.biological_reporting import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dda_biological_workflow import *  # noqa: F401,F403
+from bijux_proteomics.workflow.diann_benchmarks import *  # noqa: F401,F403
 from bijux_proteomics.workflow.diann_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.maxquant_benchmarks import *  # noqa: F401,F403
 from bijux_proteomics.workflow.maxquant_biological_workflow import *  # noqa: F401,F403
