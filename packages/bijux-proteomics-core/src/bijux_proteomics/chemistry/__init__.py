49e2d67acd17f1e3de963c6144543a16ca3850c2
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-22 20:55:41 +0200

    feat(chemistry): add modification registry engine

--- packages/bijux-proteomics-core/src/bijux_proteomics/chemistry/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/chemistry/__init__.py
@@ -9,6 +9,7 @@ from bijux_proteomics.chemistry.amino_acid_mass import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.contracts import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.fragment_ion_review import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.isotope_adduct_annotation import *  # noqa: F401,F403
+from bijux_proteomics.chemistry.modification_registry import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.modification_resolution import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.modified_peptide_conflicts import *  # noqa: F401,F403
 from bijux_proteomics.chemistry.open_search_unknown_mod import *  # noqa: F401,F403
