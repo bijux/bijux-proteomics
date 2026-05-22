f7ca61c8e69901758ba8f994d15d8da98cc54467
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-22 05:53:25 +0200

    refactor(multiplex): establish reporter ion import surface

---
+++ packages/bijux-proteomics-core/src/bijux_proteomics/multiplex/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2026 Bijan Mousavi
+
+"""Multiplex reporter-ion import, matrix, and normalization surfaces."""
+
+from __future__ import annotations
+
+from bijux_proteomics.multiplex.reporter_ion_import import *  # noqa: F401,F403
