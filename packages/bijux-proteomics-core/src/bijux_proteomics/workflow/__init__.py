e87b6e5f38d0590a60e554053427bb8624d0f8fa
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-22 11:14:41 +0200

    feat(workflow): add maxquant biological workflow bundle

--- packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
@@ -6,6 +6,7 @@ from bijux_proteomics.workflow.blueprint import *  # noqa: F401,F403
 from bijux_proteomics.workflow.biological_reporting import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dda_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.diann_biological_workflow import *  # noqa: F401,F403
+from bijux_proteomics.workflow.maxquant_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dia_differential_analysis import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dia_dda_comparison import *  # noqa: F401,F403
 from bijux_proteomics.workflow.label_based_differential_analysis import *  # noqa: F401,F403
