c879b8d7ea5e010688857c68a53506686583a4d2
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 14:15:14 +0200

    refactor(governance): audit ptm evidence card ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -215,6 +215,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "ptm/contracts.py",
             "ptm/ambiguity_handling.py",
             "ptm/context_annotation.py",
+            "ptm/evidence_cards.py",
             "ptm/localization_scoring.py",
             "ptm/motif_analysis.py",
             "ptm/occupancy_estimation.py",
