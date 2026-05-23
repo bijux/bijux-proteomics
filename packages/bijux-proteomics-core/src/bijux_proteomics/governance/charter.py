941118f9e26992cff03f117e0c17fb64078d6933
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 23:45:42 +0200

    refactor(governance): audit dia fragment coelution ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -154,6 +154,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "io/transition_table.py",
             "io/chromatographic_evidence.py",
             "io/chromatographic_peak_picking.py",
+            "io/dia_fragment_coelution.py",
             "io/retention_time_alignment.py",
             "io/xic_extraction.py",
             "identification/contracts.py",
