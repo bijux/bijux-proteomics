f73aca460fbc2c245aab555e5236f68bc3f5938f
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 09:23:37 +0200

    refactor(governance): audit power estimation ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -196,6 +196,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "quantification/missingness.py",
             "quantification/normalization.py",
             "quantification/peptide_intensity_matrix.py",
+            "quantification/power_estimation.py",
             "quantification/protein_intensity_matrix.py",
             "quantification/protein_lfq.py",
             "quantification/sample_exploration.py",
