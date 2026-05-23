8235a8a9da47f7ab30803dbd8dcab077b0e46c44
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 19:47:18 +0200

    feat(study): add owned experiment design object

--- packages/bijux-proteomics-core/src/bijux_proteomics/study/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/study/__init__.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 
 from bijux_proteomics.study.contrasts import *  # noqa: F401,F403
 from bijux_proteomics.study.contracts import *  # noqa: F401,F403
+from bijux_proteomics.study.experiment_design import *  # noqa: F401,F403
 from bijux_proteomics.study.sample_metadata import *  # noqa: F401,F403
 from bijux_proteomics.study.laboratory_operations import *  # noqa: F401,F403
 from bijux_proteomics.study.laboratory_plans import *  # noqa: F401,F403
