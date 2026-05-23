1962b56b1bbc9c4c85d468933040b895266b47a1
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 19:25:52 +0200

    feat(workflow): add biological result graph owner

--- packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from bijux_proteomics.workflow.blueprint import *  # noqa: F401,F403
 from bijux_proteomics.workflow.biological_reporting import *  # noqa: F401,F403
+from bijux_proteomics.workflow.biological_result_graph import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dda_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.diann_benchmarks import *  # noqa: F401,F403
 from bijux_proteomics.workflow.diann_biological_workflow import *  # noqa: F401,F403
