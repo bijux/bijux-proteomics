3cc50ab6e47139d86d070f86de2d5c6a0e7b6c1d
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-23 23:21:29 +0200

    feat(io): export retention time alignment surface

--- packages/bijux-proteomics-core/src/bijux_proteomics/io/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/io/__init__.py
@@ -19,6 +19,7 @@ _IO_EXPORT_MODULES = (
     "bijux_proteomics.io.target_panel",
     "bijux_proteomics.io.transition_table",
     "bijux_proteomics.io.chromatographic_peak_picking",
+    "bijux_proteomics.io.retention_time_alignment",
     "bijux_proteomics.io.xic_extraction",
 )
 
