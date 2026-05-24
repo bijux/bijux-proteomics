8c7396f380464e4e44944778095e5226ab9d11ee
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 22:07:38 +0200

    refactor(governance): audit spectrum noise estimation ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -149,6 +149,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "io/ingestion.py",
             "io/mgf_streaming.py",
             "io/mzml_reader.py",
+            "io/noise.py",
             "io/run_qc.py",
             "io/spectrum_peak_matching.py",
             "io/spectra.py",
