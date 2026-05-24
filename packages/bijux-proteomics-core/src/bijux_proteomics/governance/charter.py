9cae59ce9c4c353c57ccc4b4ce7eead5a4b1a8bd
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 22:35:02 +0200

    refactor(governance): audit false localization detection ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -263,6 +263,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "ptm/crosstalk.py",
             "ptm/evidence_cards.py",
             "ptm/fragment_scoring.py",
+            "ptm/localization_risk.py",
             "ptm/localization_scoring.py",
             "ptm/mechanism_classification.py",
             "ptm/motif_analysis.py",
