eb05f5696184d112af18503ac5c8972f067e58dc
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 09:50:05 +0200

    feat(targeted): add transition selection owner

--- packages/bijux-proteomics-core/src/bijux_proteomics/targeted/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/targeted/__init__.py
@@ -14,6 +14,7 @@ _TARGETED_EXPORT_MODULES = (
     "bijux_proteomics.targeted.result_import",
     "bijux_proteomics.targeted.target_matrix",
     "bijux_proteomics.targeted.transition_coelution",
+    "bijux_proteomics.targeted.transition_selection",
 )
 
 
