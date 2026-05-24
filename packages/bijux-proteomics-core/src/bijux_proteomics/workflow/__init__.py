45da51ae1f02c1ea85eb1ae2a4a719f68ec7122e
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 16:50:16 +0200

    feat(workflow): add interactive result bundle owner

--- packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
@@ -16,6 +16,7 @@ from bijux_proteomics.workflow.dda_biological_workflow import *  # noqa: F401,F4
 from bijux_proteomics.workflow.diann_benchmarks import *  # noqa: F401,F403
 from bijux_proteomics.workflow.diann_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.flagship_run import *  # noqa: F401,F403
+from bijux_proteomics.workflow.interactive_result_bundle import *  # noqa: F401,F403
 from bijux_proteomics.workflow.maxquant_benchmarks import *  # noqa: F401,F403
 from bijux_proteomics.workflow.maxquant_biological_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.orchestrator import *  # noqa: F401,F403
