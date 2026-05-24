7571dd296ce419f576d719d9a2ccfacf07381b37
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 03:56:00 +0200

    refactor(governance): audit ortholog site conservation ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -244,6 +244,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "ptm/evidence_cards.py",
             "ptm/localization_scoring.py",
             "ptm/motif_analysis.py",
+            "ptm/ortholog_site_conservation.py",
             "ptm/occupancy_estimation.py",
             "ptm/differential_analysis.py",
             "ptm/peptide_parser.py",
