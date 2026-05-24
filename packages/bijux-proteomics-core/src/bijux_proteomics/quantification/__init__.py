c3e363770fb7ea383d552b205f8319fdb73d9c0c
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 05:09:45 +0200

    feat(quantification): add peptide profile inconsistency owner

--- packages/bijux-proteomics-core/src/bijux_proteomics/quantification/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/quantification/__init__.py
@@ -18,6 +18,7 @@ _QUANTIFICATION_EXPORT_MODULES = (
     "bijux_proteomics.quantification.missingness",
     "bijux_proteomics.quantification.normalization",
     "bijux_proteomics.quantification.peptide_intensity_matrix",
+    "bijux_proteomics.quantification.peptide_profile_inconsistency",
     "bijux_proteomics.quantification.power_estimation",
     "bijux_proteomics.quantification.protein_intensity_matrix",
     "bijux_proteomics.quantification.protein_lfq",
