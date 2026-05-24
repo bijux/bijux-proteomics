837632a6a0e0214f7e2854b8e36b39e08a60c287
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-24 12:44:38 +0200

    feat(workflow): expose proteomics study result package surface

--- packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/workflow/__init__.py
@@ -16,6 +16,7 @@ from bijux_proteomics.workflow.ptm_site_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.protein_evidence_cards import *  # noqa: F401,F403
 from bijux_proteomics.workflow.protein_mechanism_cards import *  # noqa: F401,F403
 from bijux_proteomics.workflow.public_benchmark_runner import *  # noqa: F401,F403
+from bijux_proteomics.workflow.study_result import *  # noqa: F401,F403
 from bijux_proteomics.workflow.tmt_experiment_workflow import *  # noqa: F401,F403
 from bijux_proteomics.workflow.trust_bundle import *  # noqa: F401,F403
 from bijux_proteomics.workflow.dia_differential_analysis import *  # noqa: F401,F403
