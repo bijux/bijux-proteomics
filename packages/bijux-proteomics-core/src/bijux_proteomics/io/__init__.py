7fa05334c938e8a974f09cdc7876838db18a5a19
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-25 05:14:50 +0200

    feat(io): export input integrity surface

--- packages/bijux-proteomics-core/src/bijux_proteomics/io/__init__.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/io/__init__.py
@@ -28,6 +28,7 @@ _IO_EXPORT_MODULES = (
     "bijux_proteomics.io.precursor_validation",
     "bijux_proteomics.io.raw_signal_evidence_cards",
     "bijux_proteomics.io.chromatographic_peak_picking",
+    "bijux_proteomics.io.input_integrity",
     "bijux_proteomics.io.retention_time_alignment",
     "bijux_proteomics.io.spectral_library_intensity_agreement",
     "bijux_proteomics.io.xic_extraction",
