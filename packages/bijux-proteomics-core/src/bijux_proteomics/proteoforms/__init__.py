33db057f0689384f31a7be3f478d789d3d49d22b
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-25 02:59:52 +0200

    feat(proteoforms): add proteoforms package boundary

---
+++ packages/bijux-proteomics-core/src/bijux_proteomics/proteoforms/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2026 Bijan Mousavi
+
+"""Proteoform assembly and quantification surfaces."""
+
+from __future__ import annotations
+
+from bijux_proteomics.proteoforms.assembly import *  # noqa: F401,F403
