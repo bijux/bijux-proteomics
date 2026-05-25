4e1b27c31b21ca829f1406c89d9c3b78959a7b86
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-25 02:48:17 +0200

    refactor(governance): audit ptm acetylation analysis ownership

--- packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
+++ packages/bijux-proteomics-core/src/bijux_proteomics/governance/charter.py
@@ -272,6 +272,7 @@ DEFAULT_CORE_DOMAIN_ENTRIES: tuple[CoreDomainFamilyEntry, ...] = (
             "ptm/contracts.py",
             "ptm/ambiguity_handling.py",
             "ptm/abundance_correction.py",
+            "ptm/acetylation.py",
             "ptm/context_annotation.py",
             "ptm/crosstalk.py",
             "ptm/evidence_cards.py",
