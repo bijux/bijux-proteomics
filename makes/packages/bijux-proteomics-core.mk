42a26a1dcfa9dfbdb65a5c24d993bfa8634a2305
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-06-03 12:36:24 +0200

    fix(core-lint): allow peptide sequence tokens in codespell

--- makes/packages/bijux-proteomics-core.mk
+++ makes/packages/bijux-proteomics-core.mk
@@ -2,5 +2,6 @@ include $(abspath $(dir $(firstword $(MAKEFILE_LIST))))/../proteomics-package.mk
 
 PACKAGE_IMPORT_NAME := bijux_proteomics
 PACKAGE_INSTALL_PYTHON_PACKAGES = "$(MONOREPO_ROOT)/packages/bijux-proteomics-dev[dev]" $(MONOREPO_ROOT)/packages/bijux-proteomics-foundation $(MONOREPO_ROOT)/packages/bijux-proteomics-intelligence $(MONOREPO_ROOT)/packages/bijux-proteomics-knowledge $(MONOREPO_ROOT)/packages/bijux-proteomics-lab
+CODESPELL = $(VENV_PYTHON) -m codespell_lib -L AAS,DAA
 
 include $(abspath $(dir $(firstword $(MAKEFILE_LIST))))/../bijux-py/package.mk
